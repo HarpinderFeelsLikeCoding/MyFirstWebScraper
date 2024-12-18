a542675535f60e1c2f10d947a406cab0ffabd7cd
HarpinderSingh1999 <tui57086@temple.edu>
2024-12-17 21:21:27 -0500

    not working for bs4, some problem with the .text

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+#url of th website to scrape
+url = "https://weather.com/weather/today/l/b1449bff7d5c0f525164af0b1af9842fc0a2d930c270ed059a6220e71ee4b72a"
+
+#fetch the html content, bs4 looks for thing based off the thml structure mainly
+response = requests.get(url)
+
+#parse the html content
+soup = BeautifulSoup(response.content, "html.parser")
+
+#extract specific data
+temperature = soup.find("div", class_="HourlyForecast--adWrapper--ZMPAH").text
+condition = soup.find("div", class_="condition").string
+
+print(f"Temperature: {temperature}")
+print("Somethibgn here\n")
+print(f"Condition: {condition}")
