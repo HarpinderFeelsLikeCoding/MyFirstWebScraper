2dbe1670b4bdcf9abda86022420cf40f6e385a76
HarpinderSingh1999 <tui57086@temple.edu>
2024-12-21 10:13:07 -0500

    playwright issue has been figured out

--- trying_playwright.py
+++ trying_playwright.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto("https://github.com/HarpinderFeelsLikeCoding/MyFirstWebScraper")
+    page.goto("https://www.accuweather.com/en/us/philadelphia/19102/weather-forecast/350540")
     
     temperature = page.text_content(".temperature")
     condition = page.text_content(".condition")
@@ -12,3 +12,4 @@ with sync_playwright() as p:
     print(f"Condition: {condition}")
     browser.close()
 #playwright doesnt' work as weel as i thought
+# looks like there is another wbkit thing that is needed for playwrightto work on safari
